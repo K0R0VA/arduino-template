515751aa7d1da144b026c520b3776a566f1e57ba
mic <mic>
2026-01-14 12:05:25 +0300

    finish template

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ use panic_halt as _;
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let mut led = pins.d9.into_output_high();
+    arduino_console::init_console(dp.USART0, pins.d0, pins.d1.into_output());
+    let mut led = pins.d13.into_output_high();
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(500);
+        arduino_console::println!("hello world");
+        led.toggle().then_delay(500);
     }
 }
 
@@ -21,58 +22,3 @@ pub trait Delay {
 }
 
 impl Delay for () {}
-
-#[allow(unused)]
-mod console {
-    use arduino_hal::{hal::port::{PD0, PD1}, pac::usart0::RegisterBlock, port::{Pin, mode::{Floating, Input, Output}}};
-    use avr_device::{generic::Periph, interrupt};
-    use core::cell::RefCell;
-
-    pub fn init_console(p: Periph<RegisterBlock, 192>, 
-        rx: Pin<Input<Floating>, PD0>, 
-        tx: Pin<Output, PD1>
-    ) {
-        let console = arduino_hal::Usart::new(
-            p,
-            rx,
-            tx,
-            // See src/usart.rs for why some boards use the BaudrateArduinoExt trait
-            // instead of BaudrateExt.
-            arduino_hal::hal::usart::BaudrateArduinoExt::into_baudrate(57600),
-        );
-        interrupt::free(|cs| {
-            *CONSOLE.borrow(cs).borrow_mut() = Some(console);
-    })
-}
-
-
-    type Console = arduino_hal::hal::usart::Usart0<arduino_hal::DefaultClock>;
-    pub static CONSOLE: interrupt::Mutex<RefCell<Option<Console>>> =
-        interrupt::Mutex::new(RefCell::new(None));
-
-    #[macro_export]
-    macro_rules! print {
-        ($($t:tt)*) => {
-            avr_device::interrupt::free(
-                |cs| {
-                    if let Some(console) = crate::console::CONSOLE.borrow(cs).borrow_mut().as_mut() {
-                        let _ = ufmt::uwrite!(console, $($t)*);
-                    }
-                },
-            )
-        };
-    }
-    
-    #[macro_export]
-    macro_rules! println {
-        ($($t:tt)*) => {
-            avr_device::interrupt::free(
-                |cs| {
-                    if let Some(console) = crate::console::CONSOLE.borrow(cs).borrow_mut().as_mut() {
-                        let _ = ufmt::uwriteln!(console, $($t)*);
-                    }
-                },
-            )
-        };
-    }
-}
